e3c96aafa28d42b7839876871fe4458eaffe0d8e
OlivierParent <olivier.parent@arteveldehs.be>
2020-09-27 19:23:56 +0200

    [WIP]

---
+++ global-components/Color.vue
@@ -0,0 +1,14 @@
+<template>
+  <span :style="style"><slot /></span>
+</template>
+
+<script>
+export default {
+  created() {
+    this.style = `color: ${this.value}`;
+  },
+  props: {
+    value: String,
+  },
+};
+</script>
