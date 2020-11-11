980a68e1eacc393d8e7c6f8a9d12715aff54fc8a
OlivierParent <olivier.parent@arteveldehs.be>
2020-11-11 18:52:57 +0100

    [WIP]

--- global-components/Color.vue
+++ global-components/Color.vue
@@ -5,7 +5,30 @@
 <script>
 export default {
   created() {
-    this.style = `color: ${this.value}`;
+    let color;
+    switch (this.value) {
+      case "BLAUW":
+        color = "#00a5d9";
+        break;
+      case "GROEN":
+        color = "#5ab946";
+        break;
+      case "ORANJE":
+        color = "#f58732";
+        break;
+      case "PAARS":
+        color = "#7e57c5";
+        break;
+      case "ROOD":
+        color = "#ed0034";
+        break;
+      case "ROZE":
+        color = "#d20082";
+        break;
+      default:
+        color = this.value;
+    }
+    this.style = `color: ${color}`;
   },
   props: {
     value: String,
